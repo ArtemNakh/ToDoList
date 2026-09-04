cbb0a1690ece49edbcc1006cbc05ff85c57d2968
ArtemNakh <artem.nakhman@gmail.com>
2026-09-04 13:45:54 +0300

    added notes and tokens migrations, fix issue in migration(users)

---
+++ todolist_backend/migrations/1788517524219-tokens.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Tokens1788517524219 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE TABLE IF NOT EXISTS tokens(
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        email VARCHAR(100) NOT NULL,
+        token VARCHAR(100) NOT NULL,
+        type ENUM('VERIFICATION', 'TWO_FACTOR', 'PASSWORD_RESET') NOT NULL,
+        expires_in TIMESTAMP NOT NULL,
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    ) ENGINE=InnoDB
+        `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE IF EXISTS tokens`);
+  }
+}
---
+++ todolist_backend/migrations/1788517655218-notes.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Notes1788517655218 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+         await queryRunner.query(`CREATE TABLE IF NOT EXISTS notes (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            title VARCHAR(100) NOT NULL,
+            content_json MEDIUMTEXT NULL,
+            created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        )`)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('DROP TABLE IF EXISTS notes');
+    }
+
+}
