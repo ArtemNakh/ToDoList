b40bdead0da3350a140de4a1cf09099ac545c427
ArtemNakh <artem.nakhman@gmail.com>
2026-09-05 19:07:14 +0300

    install swagger, fix env description,fix lint issue , change ts config

--- todolist_backend/src/main.ts
+++ todolist_backend/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('TodoList API')
+    .setDescription('API для керування завданнями та нотатками')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, configSwagger);
+  const endDocsApi = '/api';
+
+  const port = configService.get<number>('APPLICATION_PORT') ?? 3000;
+
+  SwaggerModule.setup(endDocsApi, app, documentFactory);
+  await app.listen(port);
+  console.log(`🚀 Server is running on http://localhost:${port} 
+    \n Docs api: http://localhost:${port}${endDocsApi}
+    `);
 }
 await bootstrap();
